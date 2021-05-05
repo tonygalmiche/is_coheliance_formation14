ddfe7b40d9512a1a1580c6b545819b83f8e3442a
Tony Galmiche <tony.galmiche@infosaone.com>
2021-05-05 09:32:48 +0200

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+  "name" : "InfoSaône - Module Odoo 14 pour Coheliance Formation",
+  "version" : "0.1",
+  "author" : "InfoSaône / Tony Galmiche",
+  "category" : "InfoSaône",
+  "description": """
+InfoSaône - Module Odoo 14 pour Coheliance Formation
+===================================================
+
+InfoSaône - Module Odoo 14 pour Coheliance Formation
+""",
+  "maintainer": "InfoSaône",
+  "website": "http://www.infosaone.com",
+
+  "depends" : [
+    "is_coheliance14",
+  ], 
+  "init_xml" : [],            
+  "demo_xml" : [],            
+  "data" : [
+      "views/layouts.xml",
+  ],                           
+  "installable": True,         
+  "active": False,            
+  "application": True,
+}
+
+
